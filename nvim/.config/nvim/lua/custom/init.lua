3961e0baf8a12a9d794da3d24112d29008fa95b1
kola <l15567540858@gmail.com>
2022-10-03 08:04:40 +0800

    bar

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -1,30 +1,26 @@
 require("custom.options")
 require("custom.filetype")
 
-vim.g.luasnippets_path = "~/.config/nvim/lua/custom/snippets"
+-- vim.g.luasnippets_path = "~/.config/nvim/lua/custom/snippets"
+
+local has = function(x)
+	return vim.fn.has(x) == 1
+end
+
+local is_win = has("win32")
+local is_wsl = has("wsl")
+local is_mac = has("macunix")
+local is_linux = has("linux")
+
+if is_linux then
+	require("custom.linux")
+elseif is_mac then
+	vim.g.luasnippets_path = "/Users/lijialin/Library/Application Support/Code/User/snippets/"
+end
+
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	callback = function()
 		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
 	end,
 })
-
--- autocmds
--- pretty up norg ft!
-autocmd("FileType", {
-	pattern = "norg",
-	callback = function() end,
-})
-
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
-
-local is_win = has "win32"
-local is_wsl = has "wsl"
-local is_mac = has "macunix"
-local is_linux = has "linux"
-
-if is_linux then
-  require('custom.linux')
-end
