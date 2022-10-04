f203bd0d26703ff1f9591d87f3b24d79e43941ac
kola-web <l15567540858@gmail.com>
2022-10-04 22:59:19 +0800

    nvim

--- nvim/.config/nvim/lua/custom/options.lua
+++ nvim/.config/nvim/lua/custom/options.lua
@@ -2,17 +2,19 @@ local options = {
 	backup = false, -- creates a backup file
 	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
 	cmdheight = 1, -- more space in the neovim command line for displaying messages
-	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+	completeopt = { "menuone", "noselect" },
 	conceallevel = 0, -- so that `` is visible in markdown files
-	-- colorcolumn = "80",
-	-- colorcolumn = "120",
 	fileencoding = "utf-8", -- the encoding written to a file
+	foldmethod = "manual", -- folding, set to "expr" for treesitter based folding
+	foldexpr = "", -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+	guifont = "monospace:h17", -- the font used in graphical neovim applications
+	hidden = true, -- required to keep multiple buffers and open multiple buffers
 	hlsearch = true, -- highlight all matches on previous search pattern
 	ignorecase = true, -- ignore case in search patterns
 	mouse = "a", -- allow the mouse to be used in neovim
 	pumheight = 10, -- pop up menu height
 	showmode = false, -- we don't need to see things like -- INSERT -- anymore
-	showtabline = 0, -- always show tabs
+	showtabline = 2, -- always show tabs
 	smartcase = true, -- smart case
 	smartindent = true, -- make indenting smarter again
 	splitbelow = true, -- force all horizontal splits to go below current window
@@ -20,39 +22,26 @@ local options = {
 	swapfile = false, -- creates a swapfile
 	termguicolors = true, -- set term gui colors (most terminals support this)
 	timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
+	title = true, -- set the title of window to the value of the titlestring
+	-- opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
+	undodir = undodir, -- set an undo directory
 	undofile = true, -- enable persistent undo
-	updatetime = 300, -- faster completion (4000ms default)
+	updatetime = 100, -- faster completion
 	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 	expandtab = true, -- convert tabs to spaces
 	shiftwidth = 2, -- the number of spaces inserted for each indentation
 	tabstop = 2, -- insert 2 spaces for a tab
 	cursorline = true, -- highlight the current line
 	number = true, -- set numbered lines
-	relativenumber = true,
-	laststatus = 3,
-	showcmd = false,
-	ruler = false,
-	-- relativenumber = true,                  -- set relative numbered lines
 	numberwidth = 4, -- set number column width to 2 {default 4}
 	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
 	wrap = false, -- display lines as one long line
-	scrolloff = 8, -- is one of my fav
-	sidescrolloff = 8,
-	guifont = "FiraCode Nerd Font:h17", -- the font used in graphical neovim applications
-	foldmethod = "indent",
-	foldlevel = 99,
-	linespace = 6,
+	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor.
+	sidescrolloff = 8, -- minimal number of screen lines to keep left and right of the cursor.
+	showcmd = false,
+	ruler = false,
+	laststatus = 3,
 }
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.opt.fillchars.eob = " "
-
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
-
-vim.opt.shortmess:append("c")
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
