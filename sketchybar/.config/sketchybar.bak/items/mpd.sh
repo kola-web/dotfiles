782da294e6147ddb9d16841a242137ac65241eac
kola <l15567540858@gmail.com>
2022-10-08 09:40:57 +0800

    sketchybar

--- sketchybar/.config/sketchybar.bak/items/mpd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar -m --add item mpd center \
-              --set mpd update_freq=2 \
-              --set mpd script="$PLUGIN_DIR/mpd.sh" \
-              --set mpd click_script="mpc toggle"
