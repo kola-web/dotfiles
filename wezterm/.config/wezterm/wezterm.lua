ddb03aa6c0256b4a8f9d01290ddc781ac60b711f
kola <l15567540858@gmail.com>
2024-10-22 16:03:52 +0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -34,15 +34,13 @@ config.font_size = 14
 config.line_height = 1.3
 
 -- Cursor
-config.default_cursor_style = "BlinkingBar"
-config.force_reverse_video_cursor = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 -- window_background_opacity = 0.9,
 -- cell_width = 0.9,
 config.scrollback_lines = 10000
 
 if platform.is_win then
-	config.default_prog = { "pwsh" }
+	config.default_prog = { "wsl" }
 	config.window_decorations = "RESIZE|TITLE"
 else
 	config.window_decorations = "RESIZE"
