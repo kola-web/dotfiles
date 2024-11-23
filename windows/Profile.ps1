e529905cd90a7638440398121c670f17ad37d921
kola-web <l15567540858@gmail.com>
2024-11-23 12:30:40 +0800

    修复zoxide失效的问题

--- windows/Profile.ps1
+++ windows/Profile.ps1
@@ -77,13 +77,12 @@ function lnvim() {
 }
 
 # https://learn.microsoft.com/zh-cn/windows/terminal/tutorials/new-tab-same-directory
-function prompt {
+function Invoke-Starship-PreCommand {
   $loc = $executionContext.SessionState.Path.CurrentLocation;
-
-  $out = ""
-  if ($loc.Provider.Name -eq "FileSystem") {
-    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  $prompt = "$([char]27)]9;12$([char]7)"
+  if ($loc.Provider.Name -eq "FileSystem")
+  {
+    $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
   }
-  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
-  return $out
+  $host.ui.Write($prompt)
 }
