69bbf5c52d5e30541e7880d5a9749ea75c61f5c6
kola-web <l15567540858@gmail.com>
2025-01-07 14:59:05 +0800

    修复 startship和psreadline的冲突

--- windows/Profile.ps1
+++ windows/Profile.ps1
@@ -10,36 +10,12 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
+$env:STARSHIP_CONFIG = "$HOME\dotfiles\windows\starship.toml"
 $env:YAZI_FILE_ONE="C:\Program Files\Git\usr\bin\file.exe"
 $env:HTTP_PROXY="http://127.0.0.1:7897"
 $env:HTTPS_PROXY="http://127.0.0.1:7897"
 #$env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
 
-# PSReadLine
-Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
-Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory -ViMode Insert
-Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory -ViMode Command
-$OnViModeChange = [scriptblock]{
-    if ($args[0] -eq 'Command') {
-        # Set the cursor to a blinking block.
-        Write-Host -NoNewLine "`e[1 q"
-    }
-    else {
-        # Set the cursor to a blinking line.
-        Write-Host -NoNewLine "`e[5 q"
-    }
-}
-Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
-
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadlineKeyHandler -Chord "Ctrl+e" -Function ForwardChar
-Set-PSReadLineKeyHandler -Chord "Ctrl+p" -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Chord "Ctrl+n" -Function HistorySearchForward
-#Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
 # Aliases 🔗
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -71,18 +47,21 @@ function lnvim() {
 }
 
 function Invoke-Starship-PreCommand {
-    # Get the current working directory
+    # 修改窗口标题
     $currentDirectory = $pwd.Path
-
-    # Get the parent directory
     $parentDirectory = Split-Path -Parent $currentDirectory
-
-    # Get the names of the current directory and parent directory
     $currentDirectoryName = Split-Path -Leaf $currentDirectory
     $parentDirectoryName = Split-Path -Leaf $parentDirectory
-
-    # Set the console window title
     $host.ui.RawUI.WindowTitle = "$parentDirectoryName\$currentDirectoryName `a"
+
+    # 兼容 starship 分割窗口保留当前路径
+    $loc = $executionContext.SessionState.Path.CurrentLocation;
+    $prompt = "$([char]27)]9;12$([char]7)"
+    if ($loc.Provider.Name -eq "FileSystem")
+    {
+      $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+    }
+    $host.ui.Write($prompt)
 }
 
 # https://learn.microsoft.com/zh-cn/windows/terminal/tutorials/new-tab-same-directory
@@ -100,6 +79,30 @@ function Invoke-Starship-PreCommand {
 
 # plugin
 #oh-my-posh init pwsh --config "$HOME/dotfiles/windows/kola.omp.json" | Invoke-Expression
+Import-Module PSReadLine
 Invoke-Expression (&starship init powershell)
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
+Set-PSReadLineOption -EditMode Vi
+$OnViModeChange = [scriptblock]{
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    }
+    else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -PredictionSource History
+
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory -ViMode Insert
+Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory -ViMode Command
+Set-PSReadlineKeyHandler -Chord "Ctrl+e" -Function ForwardChar
+Set-PSReadLineKeyHandler -Chord "Ctrl+p" -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord "Ctrl+n" -Function HistorySearchForward
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
