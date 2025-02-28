17058998bb061fbd837a31cab0c98f68e253196a
kola-web <l15567540858@gmail.com>
2025-02-28 14:26:01 +0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,23 +6,22 @@ require("keymaps").setup(config)
 require("launch").setup(config)
 -- require("plugins").setup(config)
 
+config.audible_bell = "Disabled"
 config.color_scheme = "Gruvbox Material (Gogh)"
 
 config.enable_scroll_bar = true
 config.tab_bar_at_bottom = true
 config.window_close_confirmation = "NeverPrompt"
+
 config.font_size = 12
 config.font = wezterm.font("Maple Mono NF CN")
+
+config.enable_kitty_keyboard = true
 
 -- Cursor
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.window_background_opacity = 0.94
 
-if platform.is_win then
-	config.default_prog = { "pwsh" }
-	config.window_decorations = "RESIZE|TITLE"
-else
-	config.window_decorations = "RESIZE"
-end
+-- config.window_decorations = "RESIZE"
 
 return config
