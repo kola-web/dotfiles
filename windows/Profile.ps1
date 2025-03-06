ebb0d99dc8f6c6785f4e06ddab6ca0b139e5c778
kola-web <l15567540858@gmail.com>
2025-03-06 15:29:25 +0800

    update

--- windows/Profile.ps1
+++ windows/Profile.ps1
@@ -116,7 +116,6 @@ Set-PSReadLineKeyHandler -Chord "Ctrl+n" -Function HistorySearchForward
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 
 Import-Module -Name Microsoft.WinGet.CommandNotFound
